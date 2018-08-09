079a38655703dfbcfc5d664b82d65702541ca8ca
stanleydo <stanleydo100@gmail.com>
2018-08-09 02:22:42 -0700

    Update Download.java

--- Download.java
+++ Download.java
@@ -1,16 +1,10 @@
 package Project;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -20,8 +14,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 
 @WebServlet("/Project/Download")
 public class Download extends HttpServlet {
@@ -85,7 +77,7 @@ public class Download extends HttpServlet {
 		    {
 	        	String url = "jdbc:mysql://cs3.calstatela.edu:3306/cs3220stu07?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	            String username = "cs3220stu07";
-	            String password = "";
+	            String password = "CP2JUy!V";
 
 	            c = DriverManager.getConnection( url, username, password );
 	            Statement stmt = c.createStatement();
@@ -120,45 +112,15 @@ public class Download extends HttpServlet {
 		}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        Integer id = Integer.valueOf(request.getParameter("id"));
-//        
-//        String path = getFilePath(id);
-//        String name = getFileName(id);
-        
-//        URL url = new URL(path).openStream();
-//        File file = new File(name);
-        
-//        try (BufferedInputStream in = new BufferedInputStream(new URL(path).openStream());
-//        		  FileOutputStream fileOutputStream = new FileOutputStream(name)) {
-//        		    byte dataBuffer[] = new byte[1024];
-//        		    int bytesRead;
-//        		    while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-//        		        fileOutputStream.write(dataBuffer, 0, bytesRead);
-//        		    }
-//        		    
-//        		    in.close();
-//        		} catch (IOException e) {
-//        		    // handle exception
-//        }
-
-//        FileInputStream in = new FileInputStream(path);
-//        OutputStream out = response.getOutputStream();
-//
-//        byte buffer[] = new byte[2048];
-//        int bytesRead;
-//        while( (bytesRead = in.read( buffer )) > 0 )
-//            out.write( buffer, 0, bytesRead );
-//
-//        in.close();
     	
         Integer id = Integer.valueOf(request.getParameter("id"));
-        String path = getFilePath(id);
         String fileName = getFileName(id);
     	
         response.setContentType("application/octet-stream");
-        response.setHeader("Content-disposition", "attachment; filename=" + fileName + ".jpg");
+        response.setHeader("Content-disposition", "attachment; filename=" + fileName);
  
-        try(InputStream in = request.getServletContext().getResourceAsStream("/images/" + fileName + ".jpg");
+        //Replace "/images/" with any folder name within "www" on CS3.
+        try(InputStream in = request.getServletContext().getResourceAsStream("/images/" + fileName);
           
         	OutputStream out = response.getOutputStream()) {
  
