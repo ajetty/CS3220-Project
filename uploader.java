a58ee469873785d5ed92bd905d2c391435691f29
dtchou802 <dtchou802@gmail.com>
2018-08-08 23:18:08 -0700

    Update uploader.java

--- uploader.java
+++ uploader.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-import Lab4.Shows;
 
 import java.io.PrintWriter;
 import java.sql.Connection;
